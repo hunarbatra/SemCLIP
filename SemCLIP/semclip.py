7a26fbd0589a565a2ad47e08b3aa615938fcda50
Hunar Batra <i@hunarbatra.com>
2024-05-01 22:59:45 +0530

    add custom projection_dim for SemCLIP

--- SemCLIP/semclip.py
+++ SemCLIP/semclip.py
@@ -10,6 +10,8 @@ from .semclip_tokenization import preprocess_patches
 from .semclip_embeddings import CLIPVisionEmbeddings
 from .image_utils import convert_patches_to_pixel_values
 
+from typing import Optional
+
 
 # Load the CLIP model, processor and model config
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -17,10 +19,13 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 patch_processor = CLIPImageProcessor(do_resize=False, do_center_crop=False) # disable image resizing and center cropping
 vision_config = CLIPVisionConfig.from_pretrained("openai/clip-vit-base-patch32")
 
-def get_segments_embeddings(image_name: str, data_name: str):
+def get_segments_embeddings(image_name: str, data_name: str, projection_dim: Optional[int] = None):
     # Load image segments patches
     image_resized_patches, normalized_bbox_coords = preprocess_patches(image_name, data_name)
     image_resized_patches = convert_patches_to_pixel_values(image_resized_patches, patch_size=vision_config.patch_size, patch_processor=patch_processor)
+    
+    if projection_dim is not None: # custom projection dim, default is 512
+        vision_config.projection_dim = projection_dim
 
     # Pass image patches through custom CLIPVisionEmbeddings module
     patch_embeddings = CLIPVisionEmbeddings(vision_config, len(image_resized_patches))
@@ -55,7 +60,8 @@ def get_segments_embeddings(image_name: str, data_name: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_name", type=str, help="image file name")
-    parser.add_argument("data_name", type=str, help="data directory name")
+    parser.add_argument("--image_name", type=str, help="image file name")
+    parser.add_argument("--data_name", type=str, help="data directory name")
+    parser.add_argument("--projection_dim", type=int, default=None, help="custom projection dimension, default: 512")
     args = parser.parse_args()
-    get_segments_embeddings(args.image_name, args.data_name)
+    get_segments_embeddings(args.image_name, args.data_name, args.projection_dim)
