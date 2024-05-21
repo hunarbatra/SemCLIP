16ee155812b78997015f77d8857314305da50372
Hunar Batra <i@hunarbatra.com>
2024-05-21 02:37:03 +0100

    add datasets / models / training configs

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-dataset_config = {
+dataset_mapper = {
     'COCO-13k': 'hunarbatra/CLIP-LLaVA-Instruct-COCO-13k',
     # add more HF datasets later
+}
+
+model_mapper = {
+    'semclip-v1': 'hunarbatra/semclip-v1',
+    'semclip-v1-wd-1e-3': 'hunarbatra/semclip-v1-wd-1e-3',
+    'semclip-v1-epoch3': 'hunarbatra/semclip-v1-epoch3',
+    'clip': 'openai/clip-vit-base-patch32',
+    'eva-clip': 'QuanSun/EVA-CLIP',
+    'siglip': 'google/siglip-base-patch16-224',
+    'dinov2': 'facebook/dinov2-base',
 }
