117b408376e57171441a30c1d5b8734b5441ec87
Hunar Batra <i@hunarbatra.com>
2024-05-21 02:57:26 +0100

    add LR arg to finetuning script

--- finetune.py
+++ finetune.py
@@ -17,7 +17,7 @@ from SemCLIP.model_utils import convert_models_to_fp32, convert_models_to_fp16
 from config import dataset_mapper
 
 
-def train_model(base_model='openai/clip-vit-base-patch32', pool_type='attention', projection_dim=512, data='COCO-13k', resume_training=False, train_name='semclip-v1', batch_size=64, num_epochs=3):
+def train_model(base_model='openai/clip-vit-base-patch32', pool_type='attention', projection_dim=512, data='COCO-13k', resume_training=False, train_name='semclip-v1', batch_size=64, num_epochs=3, lr=5e-5):
     # Initialize SemCLIP
     semclip = SemCLIP(model_name=base_model, pool_type='attention', projection_dim=512, device=DEVICE)
     
@@ -28,7 +28,8 @@ def train_model(base_model='openai/clip-vit-base-patch32', pool_type='attention'
 
     semclip.model.to(DEVICE)
 
-    learning_rate = 5e-5 # huggingface trainer default lr
+    print(f'learning rate: {lr}')
+    learning_rate = lr # huggingface trainer default lr: 5e-5
     betas = (0.9, 0.999) # huggingface trainer default betas
     epsilon = 1e-6 # huggingface trainer default epsilon
     weight_decay = 0.2 # L2 regularization - finetuning CLIP with a small dataset can lead to overfitting so we add L2 regularization
@@ -150,6 +151,7 @@ if __name__ == "__main__":
     parser.add_argument('--projection_dim', type=int, default=512, help='Projection dimension for SemCLIP')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
     parser.add_argument('--num_epochs', type=int, default=3, help='Number of epochs for training') # note: huggingface trainer default num_train_epochs = 3
+    parser.add_argument('--lr', type=float, default=5e-5, help='Learning rate for training')
     
     args = parser.parse_args()
     
@@ -162,4 +164,5 @@ if __name__ == "__main__":
         train_name=args.train_name,
         batch_size=args.batch_size,
         num_epochs=args.num_epochs,
+        lr=args.lr,
     )
