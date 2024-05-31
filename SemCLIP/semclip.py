fix semclip with SemCLIPVision and SemCLIPText models to ensure gradient tracking

SemCLIP/semclip.py
@@ -1,32 +1,29 @@
 import os
-
 import torch
 import argparse
-
 import torch.nn as nn
 import numpy as np
 
-from transformers import CLIPProcessor, CLIPModel, CLIPVisionConfig, CLIPImageProcessor, CLIPTextConfig, CLIPTokenizer
-from transformers.modeling_attn_mask_utils import _create_4d_causal_attention_mask, _prepare_4d_attention_mask
+from transformers import CLIPProcessor, CLIPModel, CLIPVisionConfig, CLIPTextConfig, CLIPTokenizer, CLIPImageProcessor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from huggingface_hub import HfApi, Repository
-
-from PIL import Image
+from dotenv import load_dotenv
+from typing import Optional 
 
 from .semclip_tokenization import preprocess_patches
-from .semclip_embeddings import SemCLIPVisionEmbeddings, SemCLIPTextEmbeddings
+from .semclip_vision import SemCLIPVision
+from .semclip_text import SemCLIPText
 from .image_utils import convert_patches_to_pixel_values, DEVICE
-
-from typing import Optional
-from dotenv import load_dotenv
 
 
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
-class SemCLIP:
-    def __init__(self, model_name="openai/clip-vit-base-patch32", pool_type: str = 'attention', projection_dim: Optional[int] = None, device=DEVICE):
+class SemCLIP(nn.Module):
+    def __init__(self, model_name="openai/clip-vit-base-patch32", pool_type='attention', projection_dim=None, device=DEVICE):
+        super(SemCLIP, self).__init__() # initialize nn.Module
         self.device = device
         self.model = CLIPModel.from_pretrained(model_name, token=HF_TOKEN).to(device)
         self.tokenizer = CLIPTokenizer.from_pretrained(model_name)
@@ -36,139 +33,107 @@ class SemCLIP:
         self.text_config = CLIPTextConfig.from_pretrained(model_name)
         self.pool_type = pool_type
         
-        if projection_dim is not None:  # custom projection dim, default is 512
+        if projection_dim is not None:
             self.vision_config.projection_dim = projection_dim
-            
-        # Projection layers
-        self.visual_projection = nn.Linear(self.vision_config.hidden_size, self.vision_config.projection_dim, bias=False).to(self.device)
-        self.text_projection = nn.Linear(self.text_config.hidden_size, self.text_config.projection_dim, bias=False).to(self.device)
-        self.logit_scale = nn.Parameter(torch.tensor(self.model.config.logit_scale_init_value)).to(self.device)
+            self.text_config.projection_dim = projection_dim
+
+        self.vision_model = SemCLIPVision(self.model, self.vision_config, self.pool_type).to(device)
+        self.text_model = SemCLIPText(self.model, self.text_config, self.tokenizer, self.pool_type).to(device)
         
-    def get_segment_embeddings(self, image_name: str, data_name: str, image_file: Optional[np.ndarray] = None):
-        # Load image segments patches
+        self.text_projection = nn.Linear(self.text_config.hidden_size, self.text_config.projection_dim, bias=False).to(DEVICE)
+        self.visual_projection = nn.Linear(self.vision_config.hidden_size, self.vision_config.projection_dim, bias=False).to(DEVICE)
+
+    def get_image_features(self, image_name: str, data_name: str, image_file: Optional[np.ndarray] = None):
+        # load image segments patches and normalized bounding box coordinates
         image_resized_patches, normalized_bbox_coords = preprocess_patches(image_name, data_name, image_file=image_file)
+        # preprocess the patches (CLIPImageProcessor)
         image_resized_patches = convert_patches_to_pixel_values(image_resized_patches, patch_size=self.vision_config.patch_size, patch_processor=self.patch_processor)
-            
-        # Pass image patches through custom CLIPVisionEmbeddings module
-        patch_embeddings = SemCLIPVisionEmbeddings(self.vision_config, len(image_resized_patches)).to(self.device)
-        with torch.no_grad():
-            output_embeddings = patch_embeddings(image_resized_patches, normalized_bbox_coords)
-            
-        # Apply pre-layer normalization
-        # additional layer normalization to the combined patch & post embeddings before the transformer (clip)
-        hidden_states = self.model.vision_model.pre_layrnorm(output_embeddings)
         
-        # Pass the normalized embeddings through the CLIP transformer encoder
-        encoder_outputs = self.model.vision_model.encoder(
-            inputs_embeds=hidden_states,
-            output_attentions=False,
-            output_hidden_states=False,
-            return_dict=True,
-        )
+        pooled_output = self.vision_model(image_resized_patches, normalized_bbox_coords)
         
-        # Get the last hidden state, apply pooling and apply post-layer normalization
-        last_hidden_state = encoder_outputs.last_hidden_state
+        # pass the pooled output through a final linear projection layer
+        image_embeds = self.visual_projection(pooled_output)
         
-        if self.pool_type == 'cls':
-            pooled_output = last_hidden_state[:, 0, :]
-        elif self.pool_type == 'mean':
-            pooled_output = last_hidden_state.mean(dim=1)
-        elif self.pool_type == 'attention':
-            pooled_output = patch_embeddings.attn_pooling_head(last_hidden_state)
-        else:
-            raise ValueError(f"Invalid pooling type: {pool_type}")
-        
-        pooled_output = self.model.vision_model.post_layernorm(pooled_output)  # post layer norm
+        # image features
+        return image_embeds
+
+    def get_text_features(self, text):
+        # applies text processor, generates SemCLIPTextEmbeddings with 2D positional embeddings, and passes through the CLIP transformer encoder followed by pooling 
+        pooled_output = self.text_model(text)
         
         # Pass the pooled output through a final linear projection
-        final_embedding = self.visual_projection(pooled_output)
-        
-        # Detach the final embedding
-        final_embedding = final_embedding.detach()
-        
-        return final_embedding
+        text_embeds = self.text_projection(pooled_output)
+
+        # text features
+        return text_embeds
     
-    def get_text_embeddings(self, text: str):
-        # Tokenize the input text
-        inputs = self.tokenizer(text, return_tensors="pt").to(self.device)
-        input_ids = inputs["input_ids"]
-        attention_mask = inputs["attention_mask"]
-            
-        # Pass input_ids through custom SemCLIPTextEmbeddings module
-        text_embeddings = SemCLIPTextEmbeddings(self.text_config, self.tokenizer).to(self.device)
-        with torch.no_grad():
-            hidden_states = text_embeddings(input_ids)
-            
-        # CLIP's text model uses causal mask, prepare it here.
-        # https://github.com/openai/CLIP/blob/cfcffb90e69f37bf2ff1e988237a0fbe41f33c04/clip/model.py#L324
-        input_shape = input_ids.size()
-        causal_attention_mask = _create_4d_causal_attention_mask(
-            input_shape, hidden_states.dtype, device=hidden_states.device
-        )
-        # expand attention_mask
-        if attention_mask is not None:
-            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-            attention_mask = _prepare_4d_attention_mask(attention_mask, hidden_states.dtype)
-            
-        # Pass the hidden states through the encoder
-        encoder_outputs = self.model.text_model.encoder(
-            inputs_embeds=hidden_states,
-            attention_mask=attention_mask,
-            causal_attention_mask=causal_attention_mask,
-            output_attentions=False,
-            output_hidden_states=False,
-            return_dict=True,
-        )
-        
-        # Get the last hidden state, apply pooling and apply final layer normalization
-        last_hidden_state = encoder_outputs.last_hidden_state
-        
-        if self.pool_type == 'cls':
-            if self.model.text_model.config.eos_token_id == 2:
-                # The `eos_token_id` was incorrect before PR #24773: Let's keep what have been done here.
-                # A CLIP model with such `eos_token_id` in the config can't work correctly with extra new tokens added
-                pooled_output = last_hidden_state[
-                    torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
-                    input_ids.to(dtype=torch.int, device=last_hidden_state.device).argmax(dim=-1),
-                ]
-            else:
-                # The config gets updated `eos_token_id` from PR #24773 (so the use of exta new tokens is possible)
-                pooled_output = last_hidden_state[
-                    torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
-                    # We need to get the first position of `eos_token_id` value (`pad_token_ids` might equal to `eos_token_id`)
-                    # Note: we assume each sequence (along batch dim.) contains an  `eos_token_id` (e.g. prepared by the tokenizer)
-                    (input_ids.to(dtype=torch.int, device=last_hidden_state.device) == self.model.text_model.config.eos_token_id)
-                    .int()
-                    .argmax(dim=-1),
-                ]
-        elif self.pool_type == 'mean':
-            pooled_output = last_hidden_state.mean(dim=1)
-        elif self.pool_type == 'attention':
-            pooled_output = text_embeddings.attn_pooling_head(last_hidden_state)
+    def process_image_text(self, image, caption, image_folder=None):
+        if image_folder is None:
+            image_embedding = self.get_image_features(image_name=None, data_name=None, image_file=image)
         else:
-            raise ValueError(f"Invalid pooling type: {pool_type}")
-        
-        pooled_output = self.model.text_model.final_layer_norm(pooled_output)
-        
-        # Pass the pooled output through a final linear projection
-        final_embedding = self.text_projection(pooled_output)
-        
-        # Detach the final embedding
-        final_embedding = final_embedding.detach()
-        
-        return final_embedding
-    
-    def get_semclip_embeddings(self, images, captions, images_folder):
+            image_embedding = self.get_image_features(image_name=image, data_name=image_folder)
+        text_embedding = self.get_text_features(text=caption)
+        return image_embedding, text_embedding
+
+    def get_semclip_embeddings(self, images, captions, images_folder, multi_threading=False):
         image_embeddings = []
         text_embeddings = []
+        
+        if multi_threading:
+            max_workers = min(len(images), 64) # max workers for ThreadPoolExecutor
+            print(f'Running in parallel with {max_workers} workers')
 
-        for image, caption in zip(images, captions):
-            # Assuming get_segment_embeddings and get_text_embeddings expect a single input
-            image_embedding = self.get_segment_embeddings(image_name=image, data_name=images_folder)
-            text_embedding = self.get_text_embeddings(text=caption)
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                future_to_embeds = {executor.submit(self.process_image_text, image, caption, images_folder): (image, caption) for image, caption in zip(images, captions)}
+                
+                for future in as_completed(future_to_embeds):
+                    try:
+                        image_embedding, text_embedding = future.result()
+                        image_embeddings.append(image_embedding)
+                        text_embeddings.append(text_embedding)
+                    except Exception as e:
+                        print(f"Exception: {e}")
+        else:
+            for image, caption in zip(images, captions):
+                image_embedding = self.get_image_features(image_name=image, data_name=images_folder)
+                text_embedding = self.get_text_features(text=caption)
 
-            image_embeddings.append(image_embedding)
-            text_embeddings.append(text_embedding)
+                image_embeddings.append(image_embedding)
+                text_embeddings.append(text_embedding)
+                
+        image_embeddings = torch.cat(image_embeddings, dim=0)
+        text_embeddings = torch.cat(text_embeddings, dim=0)
+        
+        image_embeddings.to(self.device)
+        text_embeddings.to(self.device)
+
+        return image_embeddings, text_embeddings
+
+    def get_semclip_embeddings_direct_img(self, images, captions, multi_threading=False):
+        image_embeddings = []
+        text_embeddings = []
+        
+        if multi_threading:
+            max_workers = min(len(images), 64) # max workers for ThreadPoolExecutor
+            print('Running in parallel with {max_workers} workers')
+            
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                future_to_embeds = {executor.submit(self.process_image_text, image, caption): (image, caption) for image, caption in zip(images, captions)}
+                
+                for future in as_completed(future_to_embeds):
+                    try:
+                        image_embedding, text_embedding = future.result()
+                        image_embeddings.append(image_embedding)
+                        text_embeddings.append(text_embedding)
+                    except Exception as e:
+                        print(f"Exception: {e}")
+        else:
+            for image, caption in zip(images, captions):
+                image_embedding = self.get_image_features(image_name=None, data_name=None, image_file=image)
+                text_embedding = self.get_text_features(text=caption)
+
+                image_embeddings.append(image_embedding)
+                text_embeddings.append(text_embedding)
 
         image_embeddings = torch.cat(image_embeddings, dim=0)
         text_embeddings = torch.cat(text_embeddings, dim=0)
@@ -177,63 +142,45 @@ class SemCLIP:
         text_embeddings.to(self.device)
 
         return image_embeddings, text_embeddings
-    
-    def get_semclip_embeddings_direct_img(self, images, captions):
-        image_embeddings = []
-        text_embeddings = []
 
-        ctr = 0
-
-        for image, caption in zip(images, captions):
-            print(ctr)
-            ctr += 1
-            # Assuming get_segment_embeddings and get_text_embeddings expect a single input
-            image_embedding = self.get_segment_embeddings(image_name=None, data_name=None, image_file=image)
-            text_embedding = self.get_text_embeddings(text=caption)
-
-            image_embeddings.append(image_embedding)
-            text_embeddings.append(text_embedding)
-
-        image_embeddings = torch.cat(image_embeddings, dim=0)
-        text_embeddings = torch.cat(text_embeddings, dim=0)
-
-        return image_embeddings, text_embeddings
-    
-    def process_final_embeddings(self, image_embeddings, text_embeddings):
+    def forward(self, images, texts, image_folder=None, raw_embeds=False, multi_threading=False):
+        if not raw_embeds:
+            if image_folder is None:
+                image_embeddings, text_embeddings = self.get_semclip_embeddings_direct_img(images, texts, multi_threading)
+            else:
+                image_embeddings, text_embeddings = self.get_semclip_embeddings(images, texts, image_folder, multi_threading)
+        else:
+            image_embeddings = images
+            text_embeddings = texts
+            
         # Apply L2 Normalisation to normalize the embeddings to unit length
         image_embeddings = image_embeddings / image_embeddings.norm(p=2, dim=-1, keepdim=True)
         text_embeddings = text_embeddings / text_embeddings.norm(p=2, dim=-1, keepdim=True)
 
         # Compute cosine similarity as logits
-        logit_scale = self.logit_scale.exp()
+        logit_scale = self.model.logit_scale.exp()
         logits_per_text = torch.matmul(text_embeddings, image_embeddings.t()) * logit_scale
         logits_per_image = logits_per_text.t()
 
         return logits_per_image, logits_per_text
-    
-    def upload_model_to_hf_hub(self, model_name: str, hf_name: str):
+
+    def upload_model_to_hf_hub(self, model_name, hf_name):
         api = HfApi()
 
         repo_exists = api.repo_exists(repo_id=f"{hf_name}/{model_name}", token=HF_TOKEN)
-        
         print(f"Repository exists: {repo_exists}")
 
         if repo_exists:
-            # Clone the existing repository to a local directory
             repo = Repository(local_dir=model_name, clone_from=f"https://huggingface.co/{hf_name}/{model_name}", token=HF_TOKEN)
             commit_message = "Update model files"
         else:
-            # Create a new repository
             repo_url = api.create_repo(repo_id=model_name, token=HF_TOKEN, private=True)
-            # Clone the new repository to a local directory
             repo = Repository(local_dir=model_name, clone_from=repo_url, use_auth_token=HF_TOKEN)
             commit_message = "Add model files"
 
         self.model.save_pretrained(model_name)
         self.tokenizer.save_pretrained(model_name)
         self.processor.save_pretrained(model_name)
-        
-        # Push the files to the repository
         repo.push_to_hub(commit_message=commit_message)
 
 
@@ -244,8 +191,15 @@ if __name__ == "__main__":
     parser.add_argument("--pool_type", type=str, default="mean", help="pooling type; options: ['mean', 'cls', 'attention'], default: 'attention'")
     parser.add_argument("--projection_dim", type=int, default=None, help="custom projection dimension, default: 512")
     parser.add_argument("--model_name", type=str, default="openai/clip-vit-base-patch32", help="CLIP model name, default: openai/clip-vit-base-patch32")
+    parser.add_argument("--text", type=str, default="a photo of a dog", help="text input")
     args = parser.parse_args()
-    
+
     semclip = SemCLIP(model_name=args.model_name, pool_type=args.pool_type, projection_dim=args.projection_dim, device=DEVICE)
-    semclip.get_segment_embeddings(args.image_name, args.data_name)
+    image_features = semclip.get_image_features(args.image_name, args.data_name)
+    text_features = semclip.get_text_features(args.text)
+    
+    logits_per_image, logits_per_text = semclip(images=image_features, texts=text_features, raw_embeds=True)
+    
+    print(f'semclip logits_per_image: {logits_per_image}')
+    print(f'semclip logits_per_text: {logits_per_text}')
     