d766ee8a5f17fc01c73bf47b23b5bdf7b41769d1
Hunar Batra <i@hunarbatra.com>
2024-06-04 19:22:20 +0100

    fix main script for testing runs for SemCLIP

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ if __name__ == "__main__":
     parser.add_argument("--pool_type", type=str, default="attention", help="pooling type; options: ['mean', 'cls', 'attention'], default: mean")
     parser.add_argument("--projection_dim", type=int, default=None, help="custom projection dimension, default: 512")
     parser.add_argument("--model_name", type=str, default="openai/clip-vit-base-patch32", help="CLIP model name, default: openai/clip-vit-base-patch32")
+    parser.add_argument("--text", type=str, default="a photo of a dog", help="text input")
     args = parser.parse_args()
     
     semclip = SemCLIP(model_name=args.model_name, pool_type=args.pool_type, projection_dim=args.projection_dim, device=DEVICE)
-    semclip.get_segment_embeddings(args.image_name, args.data_name)
+    image_features = semclip.get_image_features(args.image_name, args.data_name, return_embeds=True)
+    text_features = semclip.get_text_features(args.text, return_embeds=True)
+    
+    logits_per_image, logits_per_text = semclip(images=image_features, texts=text_features, raw_embeds=True)
+    
+    print(f'semclip logits_per_image: {logits_per_image}')
+    print(f'semclip logits_per_text: {logits_per_text}')
