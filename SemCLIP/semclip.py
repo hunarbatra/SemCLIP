434ddcdbf790f840cb7983e006790a7421b43774
Hunar Batra <i@hunarbatra.com>
2024-06-04 17:48:04 +0100

    update semclip model arch

--- SemCLIP/semclip.py
+++ SemCLIP/semclip.py
@@ -21,70 +21,108 @@ load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
+class SemCLIPImageFeatures():
+    def __init__(self, vision_config, vision_model, visual_projection, pool_type, device=DEVICE):
+        self.device = device
+        self.vision_model = vision_model
+        self.visual_projection = visual_projection
+
+    def forward(self, image_patches, bbox_coords):
+        # SemCLIP vision model forward pass which returns the pooled output
+        pooled_output = self.vision_model.forward(image_patches, bbox_coords)
+        # pass the pooled output through a final linear projection layer
+        image_embeds = self.visual_projection(pooled_output)
+        return image_embeds
+
+class SemCLIPTextFeatures():
+    def __init__(self, text_config, tokenizer, text_model, text_projection, pool_type, device=DEVICE):
+        self.device = device
+        self.text_model = text_model
+        self.text_projection = text_projection
+
+    def forward(self, text):
+        # SemCLIP text model forward pass which returns the pooled output
+        # applies text processor, generates SemCLIPTextEmbeddings with 2D positional embeddings, and passes through the CLIP transformer encoder followed by pooling 
+        pooled_output = self.text_model.forward(text)
+        # pass the pooled output through a final linear projection layer
+        text_embeds = self.text_projection(pooled_output)
+        return text_embeds
+
 class SemCLIP(nn.Module):
     def __init__(self, model_name="openai/clip-vit-base-patch32", pool_type='attention', projection_dim=None, device=DEVICE):
-        super(SemCLIP, self).__init__() # initialize nn.Module
+        super(SemCLIP, self).__init__()
         self.device = device
         self.model = CLIPModel.from_pretrained(model_name, token=HF_TOKEN).to(device)
+        
+        # Modify specific layers
+        self.model.vision_model.embeddings.patch_embedding = nn.Linear(
+            self.model.vision_model.config.patch_size * self.model.vision_model.config.patch_size * self.model.vision_model.config.num_channels, 
+            self.model.vision_model.config.hidden_size, 
+            bias=False
+        ).to(device) # updated patch embedding layer
+        self.model.vision_model.embeddings.position_embedding = nn.Linear(4, self.model.vision_model.config.hidden_size).to(device) # updated position embedding layer for vision model with 2D positional embeddings -- 4 for [x, y, w, h] - Positional embedding layer for bbox coordinates
+        self.model.text_model.embeddings.position_embedding = nn.Linear(4, self.model.text_model.config.hidden_size).to(device) # updated position embedding layer for text model with 2D positional embeddings -- 4 for [x, y, w, h] - Positional embedding layer for token coordinates
+
         self.tokenizer = CLIPTokenizer.from_pretrained(model_name)
         self.processor = CLIPProcessor.from_pretrained(model_name)
         self.patch_processor = CLIPImageProcessor(do_resize=False, do_center_crop=False)  # disable image resizing and center cropping
-        self.vision_config = CLIPVisionConfig.from_pretrained(model_name)
-        self.text_config = CLIPTextConfig.from_pretrained(model_name)
         self.pool_type = pool_type
-        
+
         if projection_dim is not None:
-            self.vision_config.projection_dim = projection_dim
-            self.text_config.projection_dim = projection_dim
+            self.model.vision_model.config.projection_dim = projection_dim
+            self.model.text_model.config.projection_dim = projection_dim
 
-        self.vision_model = SemCLIPVision(self.model, self.vision_config, self.pool_type).to(device)
-        self.text_model = SemCLIPText(self.model, self.text_config, self.tokenizer, self.pool_type).to(device)
-        
-        self.text_projection = nn.Linear(self.text_config.hidden_size, self.text_config.projection_dim, bias=False).to(DEVICE)
-        self.visual_projection = nn.Linear(self.vision_config.hidden_size, self.vision_config.projection_dim, bias=False).to(DEVICE)
+        self.vision_model = SemCLIPVision(self.model.vision_model, self.model.vision_model.config, self.pool_type)
+        self.text_model = SemCLIPText(self.model.text_model, self.model.text_model.config, self.tokenizer, self.pool_type)
 
-    def get_image_features(self, image_name: str, data_name: str, image_file: Optional[np.ndarray] = None):
+        self.vision_features_extractor = SemCLIPImageFeatures(self.model.vision_model.config, self.vision_model, self.model.visual_projection, self.pool_type)
+        self.text_features_extractor = SemCLIPTextFeatures(self.model.text_model.config, self.tokenizer, self.text_model, self.model.text_projection, self.pool_type)
+
+    def get_image_features(self, image_name: str, data_name: str, image_file: Optional[np.ndarray] = None, return_embeds=False):
         # load image segments patches and normalized bounding box coordinates
         image_resized_patches, normalized_bbox_coords = preprocess_patches(image_name, data_name, image_file=image_file)
         # preprocess the patches (CLIPImageProcessor)
-        image_resized_patches = convert_patches_to_pixel_values(image_resized_patches, patch_size=self.vision_config.patch_size, patch_processor=self.patch_processor)
-        
-        pooled_output = self.vision_model(image_resized_patches, normalized_bbox_coords)
-        
-        # pass the pooled output through a final linear projection layer
-        image_embeds = self.visual_projection(pooled_output)
-        
-        # image features
-        return image_embeds
+        image_resized_patches = convert_patches_to_pixel_values(image_resized_patches, patch_size=self.model.vision_model.config.patch_size, patch_processor=self.patch_processor) # list of torch.tensor
 
-    def get_text_features(self, text):
-        # applies text processor, generates SemCLIPTextEmbeddings with 2D positional embeddings, and passes through the CLIP transformer encoder followed by pooling 
-        pooled_output = self.text_model(text)
-        
-        # Pass the pooled output through a final linear projection
-        text_embeds = self.text_projection(pooled_output)
+        if not return_embeds:
+            return image_resized_patches, normalized_bbox_coords
 
-        # text features
-        return text_embeds
+        image_features = self.vision_features_extractor.forward(image_resized_patches, normalized_bbox_coords)
+
+        return image_features
+
+    def get_text_features(self, text, return_embeds=False):
+        if not return_embeds:
+            return text
+
+        text_features = self.text_features_extractor.forward(text)
+
+        return text_features
     
-    def process_image_text(self, image, caption, image_folder=None):
+    def generate_image_text_embeddings(self, image, caption, image_folder):
         if image_folder is None:
-            image_embedding = self.get_image_features(image_name=None, data_name=None, image_file=image)
+            image_file = image
+            image_name = None
         else:
-            image_embedding = self.get_image_features(image_name=image, data_name=image_folder)
-        text_embedding = self.get_text_features(text=caption)
-        return image_embedding, text_embedding
+            image_file = None
+            image_name = image
+            
+        image_patches, bbox_coords = self.get_image_features(image_name=image_name, data_name=image_folder, image_file=image_file, return_embeds=False)
+        image_features = self.vision_features_extractor.forward(image_patches, bbox_coords)
+        text_features = self.text_features_extractor.forward(caption)
+            
+        return image_features, text_features
 
-    def get_semclip_embeddings(self, images, captions, images_folder, multi_threading=False):
+    def forward(self, images, texts, image_folder=None, multi_threading=False):
         image_embeddings = []
         text_embeddings = []
         
         if multi_threading:
             max_workers = min(len(images), 64) # max workers for ThreadPoolExecutor
             print(f'Running in parallel with {max_workers} workers')
-
+            
             with ThreadPoolExecutor(max_workers=max_workers) as executor:
-                future_to_embeds = {executor.submit(self.process_image_text, image, caption, images_folder): (image, caption) for image, caption in zip(images, captions)}
+                future_to_embeds = {executor.submit(self.generate_image_text_embeddings, image, caption, image_folder): (image, caption) for image, caption in zip(images, texts)}
                 
                 for future in as_completed(future_to_embeds):
                     try:
@@ -94,65 +132,14 @@ class SemCLIP(nn.Module):
                     except Exception as e:
                         print(f"Exception: {e}")
         else:
-            for image, caption in zip(images, captions):
-                image_embedding = self.get_image_features(image_name=image, data_name=images_folder)
-                text_embedding = self.get_text_features(text=caption)
-
-                image_embeddings.append(image_embedding)
-                text_embeddings.append(text_embedding)
-                
-        image_embeddings = torch.cat(image_embeddings, dim=0)
-        text_embeddings = torch.cat(text_embeddings, dim=0)
-        
-        image_embeddings.to(self.device)
-        text_embeddings.to(self.device)
-
-        return image_embeddings, text_embeddings
-
-    def get_semclip_embeddings_direct_img(self, images, captions, multi_threading=False):
-        image_embeddings = []
-        text_embeddings = []
-        
-        if multi_threading:
-            max_workers = min(len(images), 64) # max workers for ThreadPoolExecutor
-            print(f'Running in parallel with {max_workers} workers')
-            
-            with ThreadPoolExecutor(max_workers=max_workers) as executor:
-                future_to_embeds = {executor.submit(self.process_image_text, image, caption): (image, caption) for image, caption in zip(images, captions)}
-                
-                for future in as_completed(future_to_embeds):
-                    try:
-                        image_embedding, text_embedding = future.result()
-                        image_embeddings.append(image_embedding)
-                        text_embeddings.append(text_embedding)
-                    except Exception as e:
-                        print(f"Exception: {e}")
-        else:
-            for image, caption in zip(images, captions):
-                image_embedding = self.get_image_features(image_name=None, data_name=None, image_file=image)
-                text_embedding = self.get_text_features(text=caption)
-
+            for image, caption in zip(images, texts):
+                image_embedding, text_embedding = self.generate_image_text_embeddings(image, caption, image_folder)
                 image_embeddings.append(image_embedding)
                 text_embeddings.append(text_embedding)
 
-        image_embeddings = torch.cat(image_embeddings, dim=0)
-        text_embeddings = torch.cat(text_embeddings, dim=0)
-        
-        image_embeddings.to(self.device)
-        text_embeddings.to(self.device)
+        image_embeddings = torch.cat(image_embeddings, dim=0).to(self.device)
+        text_embeddings = torch.cat(text_embeddings, dim=0).to(self.device)
 
-        return image_embeddings, text_embeddings
-
-    def forward(self, images, texts, image_folder=None, raw_embeds=False, multi_threading=False):
-        if not raw_embeds:
-            if image_folder is None:
-                image_embeddings, text_embeddings = self.get_semclip_embeddings_direct_img(images, texts, multi_threading)
-            else:
-                image_embeddings, text_embeddings = self.get_semclip_embeddings(images, texts, image_folder, multi_threading)
-        else:
-            image_embeddings = images
-            text_embeddings = texts
-            
         # Apply L2 Normalisation to normalize the embeddings to unit length
         image_embeddings = image_embeddings / image_embeddings.norm(p=2, dim=-1, keepdim=True)
         text_embeddings = text_embeddings / text_embeddings.norm(p=2, dim=-1, keepdim=True)
@@ -182,7 +169,7 @@ class SemCLIP(nn.Module):
         self.tokenizer.save_pretrained(model_name)
         self.processor.save_pretrained(model_name)
         repo.push_to_hub(commit_message=commit_message)
-
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
