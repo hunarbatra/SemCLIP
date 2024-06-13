3157ecee0609090bb4968bc89ab06795ff8ad2ec
Hunar Batra <i@hunarbatra.com>
2024-06-13 04:17:13 +0100

    add simple cosine similarity scores for eval

--- evaluate.py
+++ evaluate.py
@@ -114,6 +114,96 @@ def evaluate_semclip_model(
     os.makedirs(f'experiments/{eval_run_name}', exist_ok=True)
     file_name = model_mapper[model_name].split('/')[-1]
     results_df.to_csv(f'experiments/{eval_run_name}/{file_name}.csv')
+    
+def evaluate_semclip_model_cosine(
+    # SemCLIP related parameters
+    model_name: str = 'semclip-v4', # base model i.e SemCLIP wrapper over CLIP, set to semclip variant from config to evaluate finetuned SemCLIP
+    pool_type: str = 'attention',
+    projection_dim: int = 512,
+    multi_threading: bool = False,
+    text_pos_emb_2d: bool = False,
+    # evaluation parameters
+    data: str = 'COCO-13k',
+    batch_size: int = 64,
+    data_split: str = 'test',
+    data_subset: Optional[int] = None,
+    eval_run_name: str = 'text',
+    use_finetuned: bool = False,
+    verbose: bool = False, # print semclip weights and shape to double check
+):
+    # Load SemCLIP model
+    fine_tuned_model = model_name if use_finetuned else None
+    semclip = SemCLIP(
+        model_name=model_mapper[model_name],
+        pool_type=pool_type,
+        projection_dim=projection_dim,
+        device=DEVICE,
+        text_pos_emb_2d=text_pos_emb_2d, # 1D positional embeddings for text by default
+        ignore_mismatched_sizes=True if model_name.startswith('semclip') else False,
+        fine_tuned_model=fine_tuned_model,
+        verbose=verbose,
+    ).to(DEVICE)
+    
+    # Load the dataset
+    dataset = load_dataset(dataset_mapper[data])
+    if data_subset:
+        dataset = dataset[data_split].select(range(data_subset))
+    else:
+        dataset = dataset[data_split]
+    test_loader = create_batches(dataset, batch_size)
+        
+    results = []
+    pbar = tqdm(test_loader, total=len(dataset) // batch_size)
+    
+    semclip.model.eval() # Set the model to evaluation mode
+    
+    for batch_idx, batch in enumerate(pbar):
+        predictions = []
+        
+        # Get the images and captions for the batch
+        image_batch_pil = batch['downloaded_img']
+        captions_batch = batch['caption']
+        images_names = batch['image']
+        
+        # Convert the batch of PIL images to OpenCV images
+        image_batch_cv2 = [pil_to_cv2(img) for img in image_batch_pil]
+        
+        with torch.no_grad():
+            # Get the image features / embeddings
+            image_features, label_features = semclip(
+                images=image_batch_cv2, 
+                texts=captions_batch, 
+                multi_threading=multi_threading,
+                return_features=True
+            )
+            
+            # cosine similarity score between image and text embeddings
+            logits_per_image, logits_per_text = semclip(
+                images=image_features,
+                texts=label_features,
+                raw_embeds=True
+            )
+        
+        for i, cos_score in enumerate(logits_per_image):
+            curr_score = float(cos_score[0])
+            results.append({
+                "image_name": images_names[i], 
+                "data_name": dataset_mapper[data], 
+                "model_name": model_mapper[model_name],
+                "caption": captions_batch[i], 
+                "cosine_score": curr_score
+            })
+                
+        print(f'Avg Cosine Similarity Score so far: {np.mean([r["cosine_score"] for r in results])}')
+        
+    results_df = pd.DataFrame(results)
+    
+    accuracy = np.mean(results_df['cosine_score'])
+    print(f'Avg Cosine Similarity Score: {accuracy}')
+    
+    os.makedirs(f'experiments/{eval_run_name}', exist_ok=True)
+    file_name = model_mapper[model_name].split('/')[-1]
+    results_df.to_csv(f'experiments/{eval_run_name}/{file_name}.csv')
 
 def evaluate_clip_model(
     model_name: str = 'clip',
@@ -187,6 +277,68 @@ def evaluate_clip_model(
     os.makedirs(f'experiments/{eval_run_name}', exist_ok=True)
     file_name = model_mapper[model_name].split('/')[-1]
     results_df.to_csv(f'experiments/{eval_run_name}/{file_name}.csv')
+    
+def evaluate_clip_model_cosine(
+    model_name: str = 'clip',
+    batch_size: int = 64,
+    data: str = 'COCO-13k',
+    data_split: str = 'test',
+    data_subset: Optional[int] = None,
+    eval_run_name: str = 'text',
+):
+    # Load the model and processor
+    model = AutoModel.from_pretrained(model_mapper[model_name]).to(DEVICE)
+    processor = AutoProcessor.from_pretrained(model_mapper[model_name])
+    
+    # Load the dataset
+    dataset = load_dataset(dataset_mapper[data])
+    if data_subset:
+        dataset = dataset[data_split].select(range(data_subset))
+    else:
+        dataset = dataset[data_split]
+    test_loader = create_batches(dataset, batch_size)
+    
+    results = []
+    pbar = tqdm(test_loader, total=len(dataset) // batch_size)
+    
+    model.eval() # Set the model to evaluation mode
+    
+    for batch_idx, batch in enumerate(pbar):
+        predictions = []
+        
+        # Get the images and captions for the batch
+        images_batch = batch['downloaded_img']
+        captions_batch = batch['caption']
+        images_names = batch['image']
+        
+        with torch.no_grad():
+            inputs = processor(text=captions_batch, images=images_batch, return_tensors="pt", padding=True)
+            outputs = model(input_ids=inputs["input_ids"].to(DEVICE), pixel_values=inputs["pixel_values"].to(DEVICE))
+        
+        # Get the cosine similarity scores
+        logits_per_image = outputs.logits_per_image
+        
+        # Compute the number of correct predictions
+        for i, cos_score in enumerate(logits_per_image):
+            curr_score = float(cos_score[0])
+            results.append({
+                "image_name": images_names[i], 
+                "data_name": dataset_mapper[data], 
+                "model_name": model_mapper[model_name],
+                "caption": captions_batch[i], 
+                "cosine_score": curr_score
+            })
+                
+        print(f'Avg Cosine Similarity Score so far: {np.mean([r["cosine_score"] for r in results])}')
+        
+    results_df = pd.DataFrame(results)
+    
+    accuracy = np.mean(results_df['cosine_score'])
+    print(f'Avg Cosine Similarity Score: {accuracy}')
+    
+    os.makedirs(f'experiments/{eval_run_name}', exist_ok=True)
+    file_name = model_mapper[model_name].split('/')[-1]
+    results_df.to_csv(f'experiments/{eval_run_name}/{file_name}.csv')
 
 
 if __name__ == '__main__':
@@ -204,30 +356,58 @@ if __name__ == '__main__':
     parser.add_argument('--text_pos_emb_2d', action='store_true', help='Use 2D positional embeddings for text in SemCLIP')
     parser.add_argument('--use_finetuned', action='store_true', help='Use fine-tuned SemCLIP model')
     parser.add_argument('--verbose', action='store_true', help='Print model weights and shapes') # print semclip updated layers weights and shape to double check
+    parser.add_argument('--cosine', action='store_true', help='Use direct cosine similarity for evaluation')
     
     args = parser.parse_args()
     
     if not args.semclip:
-        evaluate_clip_model(
-            model_name=args.model_name,
-            batch_size=args.batch_size,
-            data=args.data,
-            data_split=args.data_split,
-            data_subset=args.data_subset,
-            eval_run_name=args.eval_run_name,
-        )
+        if args.cosine:
+            evaluate_clip_model_cosine(
+                model_name=args.model_name,
+                batch_size=args.batch_size,
+                data=args.data,
+                data_split=args.data_split,
+                data_subset=args.data_subset,
+                eval_run_name=args.eval_run_name,
+            )
+        else:
+            evaluate_clip_model(
+                model_name=args.model_name,
+                batch_size=args.batch_size,
+                data=args.data,
+                data_split=args.data_split,
+                data_subset=args.data_subset,
+                eval_run_name=args.eval_run_name,
+            )
     else:
-        evaluate_semclip_model(
-            model_name=args.model_name,
-            pool_type=args.pool_type,
-            projection_dim=args.projection_dim,
-            multi_threading=args.multi_threading,
-            text_pos_emb_2d=args.text_pos_emb_2d,
-            data=args.data,
-            batch_size=args.batch_size,
-            data_split=args.data_split,
-            data_subset=args.data_subset,
-            eval_run_name=args.eval_run_name,
-            use_finetuned=args.use_finetuned,
-            verbose=args.verbose,
-        )
+        if args.cosine:
+            evaluate_semclip_model_cosine(
+                model_name=args.model_name,
+                pool_type=args.pool_type,
+                projection_dim=args.projection_dim,
+                multi_threading=args.multi_threading,
+                text_pos_emb_2d=args.text_pos_emb_2d,
+                data=args.data,
+                batch_size=args.batch_size,
+                data_split=args.data_split,
+                data_subset=args.data_subset,
+                eval_run_name=args.eval_run_name,
+                use_finetuned=args.use_finetuned,
+                verbose=args.verbose,
+            )
+        else:
+            evaluate_semclip_model(
+                model_name=args.model_name,
+                pool_type=args.pool_type,
+                projection_dim=args.projection_dim,
+                multi_threading=args.multi_threading,
+                text_pos_emb_2d=args.text_pos_emb_2d,
+                data=args.data,
+                batch_size=args.batch_size,
+                data_split=args.data_split,
+                data_subset=args.data_subset,
+                eval_run_name=args.eval_run_name,
+                use_finetuned=args.use_finetuned,
+                verbose=args.verbose,
+            )
+            
